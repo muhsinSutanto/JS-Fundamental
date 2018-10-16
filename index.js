a8862e693f7b9df11301320be1d6a1ff44a2c238
muhsinSutanto <sutantomuhsin@gmail.com>
2018-10-16 14:40:03 +0700

    challenge

--- index.js
+++ index.js
@@ -26,12 +26,49 @@
 // console.log(print);
 
 
+// foreach gabisa return.. jadi harus dibuat penampung
+// var array1 = ["a", "b", "c"];
+// var text = "";
 
-var array1 = ["a", "b", "c"];
-var text = "";
+// array1.forEach(function(element) {
+//   text += element + ",";
+// });
 
-array1.forEach(function(element) {
-  text += element + ",";
-});
+// console.log(text)
 
-console.log(text)
+//----------find----------------- untuk menemukan nilai didalam array yang pertama
+// var array1 = [5, 12, 8, 130, 44];
+
+// var found = array1.find(function(element) {
+//   return element > 10;
+// });
+
+// console.log(found);
+
+
+// ternery operator
+// let num = 2;
+// (num === 3) ? (console.log('true')) : (console.log('false'))
+
+const names = [
+    "Mr. Fahri",
+    "Mr. Abdul",
+    "Mrs. Josephhine",
+    "Mr. Joseph",
+    "Mr. Paul",
+    "Mrs. Paula",
+    "Mr. Hakim"
+  ];
+
+//level 0
+// let result = " ";
+  
+// let print = names.forEach(name => result += name + ',')
+// console.log(result)
+
+// let map = names.map(name => name);
+// console.log(map);
+
+//level 1
+let filter = names.filter( name => name[2] == '.')
+console.log(filter);
