cc249b0ee07e4560f0a73a87dc117a4dc3be38e3
muhsinSutanto <sutantomuhsin@gmail.com>
2018-10-16 10:31:40 +0700

    creating loop

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+for (i = 0; i<5; i++) {
+    console.log(i)
+}
+
+var data = {
+    kue : 'lupis ',
+    drink: 'jus',
+    snack: 'bolu'
+}
+
+for ( var key in data) {
+    console.log(data[key]);
+}
+
+var nomor = [7,8,9,10]
