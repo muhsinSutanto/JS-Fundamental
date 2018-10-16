9ecd9b1a4c8cb41c69c35209a14e79cacb9a2ffb
muhsinSutanto <sutantomuhsin@gmail.com>
2018-10-16 13:55:45 +0700

    .map

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
-const words = [
-    "spray",
-    "limit",
-    "elite",
-    "exuberant",
-    "destruction",
-    "present",
-    "happy"
-  ];
+// const words = [
+//     "spray",
+//     "limit",
+//     "elite",
+//     "exuberant",
+//     "destruction",
+//     "present",
+//     "happy"
+//   ];
   
-  let lengWords = words.filter (words => { 
-      console.log(words); return words.length > 6})
+//   let lengWords = words.filter (words => { 
+//       console.log(words); return words.length > 6})
 
-    let lengtword = words.filter(words => word.length > 6)
+//     let lengtword = words.filter(words => word.length > 6)
 
-console.log(lengWords)
+// console.log(lengWords)
+
+
+
+numbers = [1,4,7,9,10];
+
+var map = numbers.map( number => number * 2)
+var print = numbers.map(number => number);
+console.log(numbers)
+console.log(map)
+console.log(print);
